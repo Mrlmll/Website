8e5a8631f63039254288075686905d3660d079c6
Mrlmll <accounts@mrl-mll.de>
2025-09-27 15:04:18 +0200

    Clean Up

--- quiz/index.js
+++ quiz/index.js
@@ -1,23 +1,5 @@
 const questions = [
   {
-    question: "Was für eine Valorant Skin Reihe wäre ich?",
-    bingoName: "Valo Skin",
-    answers: [
-      { text: "Ruination", correct: false },
-      { text: "Reaver", correct: false },
-      { text: "Protocol 781-A", correct: true},
-    ] 
-  },
-  {
-    question: "Was ist mein Lieblingsspiel?",
-    bingoName: "Fav Game",
-    answers: [
-      { text: "LoL", correct: true },
-      { text: "Risk of Rain 2", correct: true },
-      { text: "R6", correct: false },
-    ] 
-  },
-  {
     question: "Wie groß bin ich?",
     bingoName: "Größe",
     answers: [
@@ -45,15 +27,6 @@ const questions = [
     ] 
   },
   {
-    question: "Lieblingssport?",
-    bingoName: "Fav Sport",
-    answers: [
-      { text: "Gym", correct: false },
-      { text: "Fahrrad", correct: true },
-      { text: "Bettsport", correct: false },
-    ] 
-  },
-  {
     question: "Lieblingsgenre?",
     bingoName: "Fav Genre",
     answers: [
@@ -63,30 +36,12 @@ const questions = [
     ] 
   },
   {
-    question: "Wie viel euro habe ich in Lol ausgegeben? Euro/Stunde?",
-    bingoName: "LoL wasted money",
-    answers: [
-      { text: "0.1€ pro H", correct: false },
-      { text: "0.02€ pro H", correct: true },
-      { text: "0.5€ pro H", correct: false },
-    ] 
-  },
-  {
     question: "Lieblingspokemon?",
     bingoName: "Fav Pokemon",
     answers: [
       { text: "Chelast", correct: true },
       { text: "Charizard", correct: false },
       { text: "Sheinux", correct: false },
-    ] 
-  },
-  {
-    question: "Spotify Minutes Last year?",
-    bingoName: "Spotify Recap",
-    answers: [
-      { text: "216.000", correct: false },
-      { text: "80.000", correct: true },
-      { text: "105.000", correct: false },
     ] 
   },
   {
@@ -108,39 +63,12 @@ const questions = [
     ] 
   },
   {
-    question: "2 lies one truth:",
-    bingoName: "2-L-o-T",
-    answers: [
-      { text: "Ich war mal moderator bei nebelniek", correct: false },
-      { text: "Ich wurde angepisst", correct: true },
-      { text: "Ich war nacktbaden mit 5 rndm menschen", correct: false },
-    ] 
-  },
-  {
-    question: "Konzert dieses Jahr",
-    bingoName: "Konzert",
-    answers: [
-      { text: "Tyler the Creator", correct: true },
-      { text: "Charlie xcx", correct: false },
-      { text: "Kendrick", correct: true },
-    ] 
-  },
-  {
     question: "Lieblingsfach?",
     bingoName: "Fav Fach",
     answers: [
       { text: "Mathe", correct: true },
       { text: "Informatik", correct: false },
       { text: "Grafikdesign", correct: false },
-    ] 
-  },
-  {
-    question: "Wo hab ich ein Video gemacht als ich 11 war?",
-    bingoName: "LetsPlay video",
-    answers: [
-      { text: "Overwatch", correct: true },
-      { text: "Minecraft", correct: false },
-      { text: "Fortnite", correct: false },
     ] 
   },
 ];
@@ -185,7 +113,7 @@ function displayQuestion() {
 }
 
 function generateBingoGrid() {
-  bingoItems = getRandomQuestions(questions, 16);
+  bingoItems = getRandomQuestions(questions, 9);
   bingoContainer.innerHTML = bingoItems.map(q => `
     <div class="bingo-item" data-bingo="${q.bingoName}">${q.bingoName}</div>
   `).join('');
@@ -200,7 +128,7 @@ function markBingoField(bingoName) {
 }
 
 function startQuiz() {
-  remainingQuestions = getRandomQuestions(questions, 16);
+  remainingQuestions = getRandomQuestions(questions, 9);
   generateBingoGrid();
   displayQuestion();
 }
