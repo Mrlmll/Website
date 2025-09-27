d5ced1ab38e4c318ee9a4cf035f05646be25ae1e
Mrlmll <accounts@mrl-mll.de>
2025-09-27 15:40:26 +0200

    test

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 $dirs = array_filter(glob('*'), 'is_dir');
 
+echo "<!DOCTYPE html><html><head><meta charset='UTF-8'><title>Meine Projekte</title></head><body>";
 echo "<h1>Meine Projekte</h1><ul>";
 foreach ($dirs as $dir) {
     echo "<li><a href='$dir/'>$dir</a></li>";
 }
-echo "</ul>";
+echo "</ul></body></html>";
 ?>
